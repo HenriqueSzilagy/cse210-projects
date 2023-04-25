c83da7c7fc338478269ba640904bd05f7f3c23a6
henriqueszilagy <henriqueszilagy@gmail.com>
2023-04-25 16:56:32 -0300

    week 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+         Job job1= new Job();
+         job1._jobTitle = "Data Analyst";
+         job1._company = "Microsoft";
+         job1._startYear = 2021;
+         job1._endYear = 2023;
+
+         Job job2= new Job();
+         job2._jobTitle = "FullStack Developer";
+         job2._company = "Google";
+         job2._startYear = 2022;
+         job2._endYear = 2023;
+
+
+         job1.DisplayJobDetails();
+         job2.DisplayJobDetails();
+
+         Resume myResume = new Resume();
+         myResume._name = "Carlos Henrique Szilagy";
+         myResume._jobs.Add(job1);
+         myResume._jobs.Add(job2);
+         
+
+        myResume.DisplayResume();
+
+         
     }
 }
