83810366021e4b4b751929a2dd46f0c536407f03
henriqueszilagy <henriqueszilagy@gmail.com>
2023-05-29 17:03:47 -0300

    finish my foudations

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,20 @@
 using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>();
+
+        // Create activities and add them to the list
+        activities.Add(new Running("Running", "03 Nov 2022", 45, 5.5));
+        activities.Add(new StationaryBicycles("Stationary Bicycles", "03 Nov 2022", 60, 25));
+        activities.Add(new Swimming("Swimming", "03 Nov 2022", 40, 15));
+
+        // Iterate through the list and display the summary of each activity
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
